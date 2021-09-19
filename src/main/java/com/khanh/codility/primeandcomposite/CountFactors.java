2ca7f3cdf5c16a570375cc840aef9090ca7b3f77
Khanh Huynh <tankhanhhuynh@gmail.com>
2021-09-19 16:42:47 +0700

    Update 19/09/2021

--- src/main/java/com/khanh/codility/primeandcomposite/CountFactors.java
+++ src/main/java/com/khanh/codility/primeandcomposite/CountFactors.java
@@ -8,7 +8,7 @@ package com.khanh.codility.primeandcomposite;
  */
 public class CountFactors {
 
-	// 71%
+	// 71% - O(n)
 //	public int solution(int N) {
 //		int count = 0;
 //		for (int i = 1; i <= N; i++) {
@@ -19,14 +19,17 @@ public class CountFactors {
 //		return count;
 //	}
 
+	// 100% - O(sqrt(n))
 	public int solution(int N) {
 		int count = 0;
 		double squareRoot = Math.sqrt(N);
 		for (int i = 1; i <= squareRoot; i++) {
 			if (N % i == 0) {
-				if (i < squareRoot) { // there is a opposite value of i so + 2
+				if (i < squareRoot) {
+					// there is a opposite value of i => count+=2 , ex 16 = 1*16, 2*8 
 					count += 2;
 				} else {
+					// 4*4 (just count 1 because sqrt(16) = 4
 					count++;
 				}
 			}
